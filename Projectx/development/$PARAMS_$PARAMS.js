e06fc80ff6d6bb5e89e931b12b1bacfc582adaa1
KIMHYUNBIN <kimhunbin0226@gmail.com>
2024-04-14 12:52:44 +0900

    first commit

---
+++ Projectx/development/$PARAMS_$PARAMS.js
@@ -0,0 +1,9 @@
+if (!window.$PARAMS) { window.$PARAMS = {} };Object.mixin(window.$PARAMS, {
+    "testOffline": true,
+    "resolution": {
+        "width": 1280,
+        "height": 720
+    },
+    "fontFamilies": [],
+    "platform": {}
+});
