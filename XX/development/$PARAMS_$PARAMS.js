0e2e364fec8c0fbd90e91a3b27999e5490b9158f
KIMHYUNBIN <kimhunbin0226@gmail.com>
2024-04-17 18:29:31 +0900

    대화시스템 어떻게 하는지 알았음, 여러가지 효과 체험중

--- XX/development/$PARAMS_$PARAMS.js
+++ XX/development/$PARAMS_$PARAMS.js
@@ -1,9 +1,10 @@
 if (!window.$PARAMS) { window.$PARAMS = {} };Object.mixin(window.$PARAMS, {
-    "testOffline": true,
     "resolution": {
         "width": 1280,
         "height": 720
     },
     "fontFamilies": [],
+    "testOffline": true,
+    "isOffline": true,
     "platform": {}
 });
